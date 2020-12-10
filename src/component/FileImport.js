aeeef1df1f5cce3965da52d378e3bb338ef745c3
doanhnguyen99 <doanh080199@gmail.com>
2020-12-10 19:24:59 +0700

    import file

--- src/component/FileImport.js
+++ src/component/FileImport.js
@@ -1,36 +1,56 @@
 import React, {Component} from 'react'
 import './css/style.css'
-import { Form } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 
 class FileImport extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            predictResult: ''
+        }
+    }
+
     render(){
         return(
             <div>
-                {/* <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#news">Text Input</a></li>
-                    <li><a href="#contact">Import File</a></li>
-                </ul> */}
-
                 <div className="title-text">
                     <h3>CHỌN FILE ĐỂ DỰ ĐOÁN</h3>
                 </div>
 
                 <div className="container">
                     <div className="row text-input">
-                        <form action="" method="">
-                            {/* <input type="file" /> */}
-                            <Form>
+                            <Form onSubmit={(data)=>{
+                                console.log(data.target.file.files[0]);
+                                
+                                fetch('http://127.0.0.1:5000/filejson', {
+                                    method: "POST",
+                                    headers: {
+                                        "Content-Type": "multipart/form-data",
+                                        "Accept": "multipart/form-data"
+                                        // "type": "formData"
+                                
+                                    },
+                                    body: {file: data.target.file.files[0]}
+                                }).then(response => response.json()
+                                    ).then(result => {
+                                        console.log(result.result)
+                                        this.setState({
+                                            predictResult: result.result
+                                        });
+                                    }
+                                    );
+                                    data.preventDefault();
+                            }}>
                                 <Form.Group>
-                                    <Form.File id="exampleFormControlFile1" label="Import file input" />
+                                    <Form.File id="exampleFormControlFile1"  name="file"  label="Chọn file...." />
                                 </Form.Group>
+                                    <button className="button" type="submit">Dự đoán</button>
                             </Form>
                             <br></br>
-                            <button className="button">Dự đoán</button>
-                        </form>
+                           
+                            <h3 className="title-text">Kết quả: <h2>{this.state.predictResult}</h2></h3>
                     </div>
                 </div>
-
             </div>
         );
     }
